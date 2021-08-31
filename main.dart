0df41de1ee3fe5d9665c8abbec763b0f9425d180
lukman <eniolaadeniyi86@gmail.com>
2021-08-31 12:12:41 +0100

    first-practise on dart

---
+++ main.dart
@@ -0,0 +1,64 @@
+import 'dart:math';
+import 'dart:io';
+/*
+
+main(){
+
+
+  String firstName = 'jim';
+  String age = "74";
+
+  print("there is a man named ${firstName}");
+  print ("he was ${age}years old");
+
+   firstName = "lukman";
+  print("he really liked the name ${firstName}");
+  print("but didn;t like being ${age}");
+
+
+
+
+}
+
+ */
+
+main(){
+ /* String  FirstName = "colman";
+  int Age = 26;
+  double cga= 3.5;
+  bool IsRegisteredVoter= false;
+
+  String greeting2 =" world";
+
+  String greeting = "hello";
+ //  print(
+   //    greeting.indexOf("l"));
+   /*print (greeting[4]);
+   print (greeting.contains("7"));
+   print( "the greeting is: ${greeting + greeting2}" );
+   print("the greeting is: ${greeting}${greeting2}");
+   */
+ /* int quantity= 300;
+  double price = 5.99;
+   print(5*3);
+   print(5+3 * (2+4-4+5));
+   print(min (9,7));
+   print(
+     max(100,200)
+   );
+print(
+  sqrt(64)
+);*/
+//print(5>3);
+
+*/
+print("what is your name?:");
+String? UserName= stdin.readLineSync();
+print(
+  "hello ${UserName}"
+);
+
+
+
+
+}
